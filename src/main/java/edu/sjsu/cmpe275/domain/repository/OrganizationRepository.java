00c7dc67f0a74743b24fc825a6d8700aeea58f18
Mayur Barge <bargemayur05@gmail.com>
2019-05-07 12:02:07 -0700

    Custom JpaRepository queries for org and membership

--- src/main/java/edu/sjsu/cmpe275/domain/repository/OrganizationRepository.java
+++ src/main/java/edu/sjsu/cmpe275/domain/repository/OrganizationRepository.java
@@ -4,7 +4,10 @@ import edu.sjsu.cmpe275.domain.entity.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
     List<Organization> findByNameContainingIgnoreCase(String name);
+
+    Optional<Organization> findByOwnerId(Long owner_id);
 }
