5bfb1ae5dc3fe23a938cffbdad4ee73e3df3123d
Mayur Barge <bargemayur05@gmail.com>
2019-05-06 22:22:43 -0700

    Service layer changes for membership and org

--- src/main/java/edu/sjsu/cmpe275/service/OrganizationService.java
+++ src/main/java/edu/sjsu/cmpe275/service/OrganizationService.java
@@ -1,6 +1,7 @@
 package edu.sjsu.cmpe275.service;
 
 import edu.sjsu.cmpe275.domain.entity.Organization;
+import edu.sjsu.cmpe275.domain.entity.OrganizationMembership;
 import edu.sjsu.cmpe275.domain.entity.User;
 import edu.sjsu.cmpe275.domain.exception.OrganizationNotFoundException;
 import edu.sjsu.cmpe275.domain.repository.OrganizationRepository;
@@ -8,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 public class OrganizationService {
@@ -16,13 +20,17 @@ public class OrganizationService {
 
     private UserService userService;
 
+    private OrganizationMembershipService organizationMembershipService;
+
     @Autowired
     public OrganizationService(
             final OrganizationRepository organizationRepository,
-            final UserService userService
+            final UserService userService,
+            final OrganizationMembershipService organizationMembershipService
     ) {
         this.organizationRepository = organizationRepository;
         this.userService = userService;
+        this.organizationMembershipService = organizationMembershipService;
     }
 
     public List<Organization> findOrganizations() {
@@ -42,5 +50,16 @@ public class OrganizationService {
         User owner = userService.findUser(ownerId);
         organization.setOwner(owner);
         return organizationRepository.save(organization);
+    }
+
+    public List<User> findApprovedOrganizationMembers(final Long id) {
+        Organization organization = findOrganization(id);
+        List<OrganizationMembership> approvedOrganizationMemberShipList =
+                organizationMembershipService.findApprovedOrganizationMemberships(organization);
+        return Objects.nonNull(approvedOrganizationMemberShipList)
+                ? approvedOrganizationMemberShipList
+                .stream()
+                .map(organizationMembership -> userService.findUser(organizationMembership.getMember().getId()))
+                .collect(Collectors.toList()) : new ArrayList<>();
     }
 }
