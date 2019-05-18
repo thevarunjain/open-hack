cd214341811f6bc6b178970c1eb41ac0c460d930
Mayur Barge <bargemayur05@gmail.com>
2019-05-18 15:30:40 -0700

    Will add tests later

--- src/test/java/edu/sjsu/cmpe275/service/HackathonServiceUnitTest.java
+++ src/test/java/edu/sjsu/cmpe275/service/HackathonServiceUnitTest.java
@@ -1,7 +1,5 @@
 package edu.sjsu.cmpe275.service;
 
-import static org.junit.Assert.*;
-
 public class HackathonServiceUnitTest {
 
 }
--- src/test/java/edu/sjsu/cmpe275/web/HackathonControllerIntegrationTest.java
+++ src/test/java/edu/sjsu/cmpe275/web/HackathonControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package edu.sjsu.cmpe275.web;
 
-import static org.junit.Assert.*;
-
 public class HackathonControllerIntegrationTest {
 
 }
